ac17652c46f9471691c6763a00aeb01f38e025dc
banagere <past.ice1932@banagere.com>
2022-07-13 11:02:39 +0530

    Added MDX

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
 
-module.exports = nextConfig
+module.exports = withMDX({
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+});
