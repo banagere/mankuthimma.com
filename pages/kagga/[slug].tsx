e74e160c5e2f516a04df8fc3c59fa780eabbd9fc
banagere <past.ice1932@banagere.com>
2022-07-27 14:16:04 +0530

    added link to headings and footnote

--- pages/kagga/[slug].tsx
+++ pages/kagga/[slug].tsx
@@ -6,6 +6,8 @@ import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
 import { serialize } from "next-mdx-remote/serialize";
 import rehypeSlug from "rehype-slug";
 import rehypeHighlight from "rehype-highlight";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import remarkGfm from "remark-gfm";
 import { getPostFromSlug, getSlugs, PostMeta } from "../../components/api";
 
 interface MDXPost {
@@ -20,13 +22,16 @@ export default function PostPage({ post }: { post: MDXPost }) {
         <title>{post.meta.title}</title>
       </Head>
 
-      <span className="mt-2 font-semibold text-bronze-600 hover:text-bronze-400">
+      <span className="font-semibold text-bronze-600 hover:text-bronze-400">
         <Link href="/">← Home</Link>
       </span>
 
-      <h1 className="mt-5 text-3xl font-bold">
-        {post.meta.title}
-      </h1>
+      <div className="">
+        <h1 className="mt-5 text-2xl font-bold">{post.meta.title}</h1>
+        <p className="text-lg font-semibold text-neutral-500">
+          {post.meta.number}
+        </p>
+      </div>
 
       <article className="pt-5 prose prose-h1:text-2xl prose-h2:text-xl prose-h2:text-gray-400 prose-h3:text-lg prose-h3:text-bronze-500 prose-h3:mt-2 prose-headings:m-0 prose-img:rounded-md">
         <MDXRemote {...post.source} components={{ Image }} />
@@ -40,7 +45,19 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { content, meta } = getPostFromSlug(slug);
   const mdxSource = await serialize(content, {
     mdxOptions: {
-      rehypePlugins: [rehypeSlug, rehypeHighlight],
+      rehypePlugins: [
+        rehypeSlug,
+        rehypeHighlight,
+        [
+          rehypeAutolinkHeadings,
+          {
+            properties: {
+              className: ["anchor"],
+            },
+          },
+        ],
+      ],
+      remarkPlugins: [remarkGfm],
     },
   });
 
