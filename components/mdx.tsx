8aaadf4bf0da2e2b264a335c4f119f6821c72ebc
Ravi Banagere <79131947+banagere@users.noreply.github.com>
2024-03-06 09:53:23 +0530

    cleaned up code

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,44 +1,7 @@
 import * as React from "react";
-import Link from "next/link";
-import Image from "next/image";
 import { useMDXComponent } from "next-contentlayer/hooks";
 
-const CustomLink = (props) => {
-  const href = props.href;
-
-  if (href.startsWith("/")) {
-    return (
-      <Link href={href} {...props}>
-        {props.children}
-      </Link>
-    );
-  }
-
-  if (href.startsWith("#")) {
-    return <a {...props} />;
-  }
-
-  return <a target="_blank" rel="noopener noreferrer" {...props} />;
-};
-
-function RoundedImage(props) {
-  return <Image alt={props.alt} className="rounded-lg" {...props} />;
-}
-
-function Callout(props) {
-  return (
-    <div className="flex p-4 my-8 border rounded-lg bg-neutral-100 border-neutral-200 ">
-      <div className="flex items-center w-4 mr-4">{props.emoji}</div>
-      <div className="w-full callout">{props.children}</div>
-    </div>
-  );
-}
-
-const components = {
-  Image: RoundedImage,
-  a: CustomLink,
-  Callout,
-};
+const components = {};
 
 interface MdxProps {
   code: string;
