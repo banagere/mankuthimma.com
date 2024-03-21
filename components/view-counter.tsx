59deb929d02784a41ddcfdcec09f041624323898
Ravi Banagere <79131947+banagere@users.noreply.github.com>
2024-03-21 23:15:19 +0530

    cleaned `view-counter`

--- components/view-counter.tsx
+++ components/view-counter.tsx
@@ -1,18 +1,5 @@
 "use client";
 
-// import { kv } from "@vercel/kv";
-
-// export default async function ViewCounter() {
-//   const views = await kv.incr("views");
-
-//   return (
-//     <p className="text-sm text-gray-500">
-//       {Intl.NumberFormat("en-us").format(views)} views
-//     </p>
-//   );
-// }
-
-// Specific slug
 import { useEffect, useState } from "react";
 
 export default function ViewCounter({ slug }) {
@@ -21,7 +8,7 @@ export default function ViewCounter({ slug }) {
   useEffect(() => {
     // Function to fetch the view count
     const fetchViews = async () => {
-      const response = await fetch(`/api/view-counter/${slug}`);
+      const response = await fetch(`/api/views/${slug}`);
       if (response.ok) {
         const data = await response.json();
         setViews(data.views);
