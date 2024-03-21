2010f042a6af06aff6ade32003098334aee596e4
Ravi Banagere <79131947+banagere@users.noreply.github.com>
2024-03-21 23:28:03 +0530

    empty

--- app/kagga/[slug]/page.tsx
+++ app/kagga/[slug]/page.tsx
@@ -45,7 +45,9 @@ export default async function Kagga({ params }) {
           <h1 className="text-2xl font-semibold">{post.title}</h1>
           <p className="font-semibold tracking-wider text-red-700">
             {post.number}
-            <PostMetrics slug={post.slug} />
+            {/* <Suspense>
+              <ViewCounter slug={post.slug} />
+            </Suspense> */}
           </p>
 
           <Mdx code={post.body.code} />
